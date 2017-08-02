06bf6b7e06677c2b03d27dc5532e046883947824
sam961124 <sam961124@gmail.com>
2017-08-02 16:45:33 +0800

    index update

---
+++ javascripts/controller.js
@@ -0,0 +1,10 @@
+//load html
+$(function(){
+    $("#navbar").load("views/navbar.html");
+    $("#header").load("views/header.html");
+});
+
+//animation
+$(function(){
+    $("#header").addClass("animated fadeInUp");
+});
